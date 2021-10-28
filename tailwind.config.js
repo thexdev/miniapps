a01b3e4b19227678214779edaf4786e7b6ab12fb
M. Akbar Nugroho <akbarnugroho253@gmail.com>
2021-10-28 09:06:39 +0700

    feat: add fav icon and font

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
+    fontFamily: {
+      sans: ['Zen Kaku Gothic Antique', 'sans-serif'],
+    },
   },
   variants: {
     extend: {},
